218e46e002a667d13c4186bd9269b529ed430510
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-05-11 23:08:24 -0600

    Querys principales de BD

---
+++ Super Los Patitos/Base de Datos/Stored Procedures.sql
@@ -0,0 +1,11 @@
+-- Aquí se van a colocar todos los stored procedures que vayan a ocupar en el codigo
+-- Pueden ejecutar los querys desde el propio Visual Studio ;)
+
+-- Nombre de Stored Procedure y descripción
+-- {CODIGO DE SQL}
+
+-- Nombre de Stored Procedure y descripción
+-- {CODIGO DE SQL}
+
+-- Nombre de Stored Procedure y descripción
+-- {CODIGO DE SQL}
