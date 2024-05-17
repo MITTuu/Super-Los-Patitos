495e1754d06b290e6a61bd10089769dac290a89b
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-05-17 01:33:52 -0600

    Base para gráficos

--- Prototipo/Prototipo/Graficos.Designer.cs
+++ Prototipo/Prototipo/Graficos.Designer.cs
@@ -30,52 +30,58 @@
         {
             this.pnGraficos = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.pVisualizadorGraficos = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnGrafico1 = new System.Windows.Forms.Button();
             this.pnGraficos.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.pVisualizadorGraficos.SuspendLayout();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnGraficos
             // 
             this.pnGraficos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnGraficos.Controls.Add(this.btnGrafico1);
             this.pnGraficos.Controls.Add(this.label3);
-            this.pnGraficos.Controls.Add(this.panel3);
+            this.pnGraficos.Controls.Add(this.pVisualizadorGraficos);
             this.pnGraficos.Controls.Add(this.panel4);
-            this.pnGraficos.Location = new System.Drawing.Point(210, 68);
+            this.pnGraficos.Location = new System.Drawing.Point(30, 22);
+            this.pnGraficos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnGraficos.Name = "pnGraficos";
-            this.pnGraficos.Size = new System.Drawing.Size(1314, 697);
+            this.pnGraficos.Size = new System.Drawing.Size(986, 567);
             this.pnGraficos.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(106, 158);
+            this.label3.Location = new System.Drawing.Point(84, 254);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 29);
+            this.label3.Size = new System.Drawing.Size(48, 24);
             this.label3.TabIndex = 1;
             this.label3.Text = "Tipo";
             // 
-            // panel3
+            // pVisualizadorGraficos
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(273, 27);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1041, 669);
-            this.panel3.TabIndex = 2;
+            this.pVisualizadorGraficos.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pVisualizadorGraficos.Controls.Add(this.label2);
+            this.pVisualizadorGraficos.Location = new System.Drawing.Point(205, 22);
+            this.pVisualizadorGraficos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pVisualizadorGraficos.Name = "pVisualizadorGraficos";
+            this.pVisualizadorGraficos.Size = new System.Drawing.Size(781, 544);
+            this.pVisualizadorGraficos.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(512, 286);
+            this.label2.Location = new System.Drawing.Point(384, 232);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 29);
+            this.label2.Size = new System.Drawing.Size(69, 24);
             this.label2.TabIndex = 0;
             this.label2.Text = "Gráfico";
             // 
@@ -85,8 +91,9 @@
             this.panel4.Controls.Add(this.label4);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1312, 30);
+            this.panel4.Size = new System.Drawing.Size(984, 24);
             this.panel4.TabIndex = 1;
             // 
             // label4
@@ -94,26 +101,41 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Location = new System.Drawing.Point(2, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 22);
+            this.label4.Size = new System.Drawing.Size(65, 18);
             this.label4.TabIndex = 0;
             this.label4.Text = "Gráficos";
             // 
+            // btnGrafico1
+            // 
+            this.btnGrafico1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnGrafico1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrafico1.Location = new System.Drawing.Point(5, 39);
+            this.btnGrafico1.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrafico1.Name = "btnGrafico1";
+            this.btnGrafico1.Size = new System.Drawing.Size(196, 41);
+            this.btnGrafico1.TabIndex = 7;
+            this.btnGrafico1.Text = "Ventas";
+            this.btnGrafico1.UseVisualStyleBackColor = false;
+            this.btnGrafico1.Click += new System.EventHandler(this.btnGrafico1_Click);
+            // 
             // Graficos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1732, 1053);
+            this.ClientSize = new System.Drawing.Size(1053, 627);
             this.Controls.Add(this.pnGraficos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Graficos";
             this.Text = "Graficos";
             this.pnGraficos.ResumeLayout(false);
             this.pnGraficos.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.pVisualizadorGraficos.ResumeLayout(false);
+            this.pVisualizadorGraficos.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.ResumeLayout(false);
@@ -124,9 +146,10 @@
 
         private System.Windows.Forms.Panel pnGraficos;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel pVisualizadorGraficos;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGrafico1;
     }
 }
--- Prototipo/Prototipo/Graficos.cs
+++ Prototipo/Prototipo/Graficos.cs
@@ -16,5 +16,36 @@ namespace Prototipo.Prototipo
         {
             InitializeComponent();
         }
+
+        
+
+        //Abrir otro form pero en un form 
+        private void abrir_Ventana(object nuevaVentana)
+        {
+            //Verifica si hay ventanas abiertas en panelOperaciones
+            if (this.pVisualizadorGraficos.Controls.Count > 0)
+            {
+                this.pVisualizadorGraficos.Controls.RemoveAt(0);
+            }
+
+            //Crea el objeto de la ventana y define unas propiedades
+            Form f = nuevaVentana as Form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill;
+
+            //Agrega la ventana al panelOperaciones
+            this.pVisualizadorGraficos.Controls.Add(f);
+            this.pVisualizadorGraficos.Tag = f;
+            f.Show();
+        }
+
+        // ------------------------------------------
+        // EVENTOS DE BOTONES PARA ACCEDER A GRAFICOS
+        // ------------------------------------------
+
+        private void btnGrafico1_Click(object sender, EventArgs e)
+        {
+            abrir_Ventana(new GraficoVentas());
+        }
     }
 }
